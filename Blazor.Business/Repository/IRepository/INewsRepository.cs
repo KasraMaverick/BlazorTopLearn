0da8664be4f39ec5695c80bfd496a53a389fac5b
nadri.k <nadri.k@VDI-DOTNET2-18>
2024-02-17 10:32:40 +0330

    Vid 26 - Full (Added INewsRepository)

---
+++ Blazor.Business/Repository/IRepository/INewsRepository.cs
@@ -0,0 +1,23 @@
+﻿using Blazor.Model.NewsDtos;
+
+namespace Blazor.Business.Repository.IRepository
+{
+    public interface INewsRepository
+    {
+        public Task<NewsDTO> CreateNews(NewsDTO newsDTO);
+
+        public Task<NewsDTO> UpdateNews(int id, NewsDTO newsDTO);
+
+        public Task<NewsDTO> GetNewsById(int newsId);
+
+        public Task<IEnumerable<NewsDTO>> GetAllNews();
+
+        public Task<NewsDTO> NewsExistsByTitle(string title);
+
+        public Task<NewsDTO> RemoveNews(int newsId);
+
+        public Task<NewsDTO> RemoveNews(NewsDTO news);
+
+
+    }
+}
---
+++ Blazor.Business/Repository/NewsRepository.cs
@@ -0,0 +1,50 @@
+﻿using Blazor.Business.Repository.IRepository;
+using Blazor.Data.Context;
+using Blazor.Model.NewsDtos;
+
+namespace Blazor.Business.Repository
+{
+    public class NewsRepository : INewsRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public NewsRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<NewsDTO> CreateNews(NewsDTO newsDTO)
+        {
+
+            await _context.News.AddAsync();
+        }
+
+        public Task<IEnumerable<NewsDTO>> GetAllNews()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<NewsDTO> GetNewsById(int newsId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<NewsDTO> NewsExistsByTitle(string title)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<NewsDTO> RemoveNews(int newsId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<NewsDTO> RemoveNews(NewsDTO news)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<NewsDTO> UpdateNews(int id, NewsDTO newsDTO)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
