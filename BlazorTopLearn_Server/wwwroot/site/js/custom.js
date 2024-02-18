1045619d77198cbf8d12226ae011dd17aba4cd62
nadri.k <nadri.k@VDI-DOTNET2-18>
2024-02-18 11:22:05 +0330

    Vid 35 - Full (Added Toastr)

---
+++ BlazorTopLearn_Server/wwwroot/site/js/custom.js
@@ -0,0 +1,8 @@
+﻿window.ShowToastr = (type, message) => {
+    if (type === 'success') {
+        toastr.success(message, 'عملیات مورد نظر با موفقیت انجام شد');
+    }
+    if (type === 'error') {
+        toastr.error(message, 'عملیات با شکست مواجه شد');
+    }
+}
