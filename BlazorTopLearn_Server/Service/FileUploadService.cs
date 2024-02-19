3b2c097b72b06e28f44ed5aa15254f803cae21f5
nadri.k <nadri.k@VDI-DOTNET2-18>
2024-02-19 09:37:44 +0330

    Fixed DeleteFile in FileUploadService

--- BlazorTopLearn_Server/Service/FileUploadService.cs
+++ BlazorTopLearn_Server/Service/FileUploadService.cs
@@ -20,6 +20,8 @@ namespace BlazorTopLearn_Server.Service
                     File.Delete(path);
                     return true;    
                 }
+
+                return false;
             }
             catch (Exception e)
             {
