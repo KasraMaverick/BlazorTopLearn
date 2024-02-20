832c37b547844e5f7f4040ec900cf3cbae2e9f1d
nadri.k <nadri.k@VDI-DOTNET2-18>
2024-02-20 12:42:15 +0330

    Vid 49 - Full (Added Register and Login)

--- BlazorTopLearn_Server/Program.cs
+++ BlazorTopLearn_Server/Program.cs
@@ -14,13 +14,15 @@ builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<ApplicationDbContext>();
+
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders().AddDefaultUI();
 
 builder.Services.AddAutoMapper(typeof(MappingConfig));
 
